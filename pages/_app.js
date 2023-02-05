13c8c5061573e275ffa2632cb4b9c4b2808706ef
Rehan41302 <saeedrehan3@gmail.com>
2023-02-05 16:22:35 +0500

    adding Google Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,38 @@
-import Head from "next/head";
 import { Fragment, useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import Head from "next/head";
+
 import PreLoader from "../src/layout/PreLoader";
 import "../styles/glitch.css";
 import "../styles/globals.css";
 
+import * as ga from '../lib/ga'
+
 function MyApp({ Component, pageProps }) {
   const [load, setLoad] = useState(true);
+  const router = useRouter()
+
   useEffect(() => {
     setTimeout(() => {
       setLoad(false);
     }, 1000);
   }, []);
+
+  // Handling logs for Google Analytics
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      ga.pageview(url)
+    }
+    //When the component is mounted, subscribe to router changes
+    //and log those page views
+    router.events.on('routeChangeComplete', handleRouteChange)
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
 
   return (
     <Fragment>
--- pages/_document.js
+++ pages/_document.js
@@ -1,11 +1,29 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html>
       <Head>
+        {/* Global Site Tag (gtag.js) - Google Analytics */}
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+          `,
+          }}
+        />
+
         {/* <!-- Favicon --> */}
-      <link rel="shortcut icon" type="image/x-icon" href="/favicon.png" />
+        <link rel="shortcut icon" type="image/x-icon" href="/favicon.png" />
         {/* <!-- plugin CSS --> */}
         <link
           href="static/plugin/bootstrap/css/bootstrap.min.css"
@@ -49,5 +67,5 @@ export default function Document() {
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
