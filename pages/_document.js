bbf24bd8cd881a81b95f1d826f63088a83001f0f
Rehan Saeed <43314251+Rehan41302@users.noreply.github.com>
2023-02-15 21:28:37 +0500

    adding smart look analytics

--- pages/_document.js
+++ pages/_document.js
@@ -22,6 +22,19 @@ export default function Document() {
           }}
         />
 
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+          window.smartlook||(function(d) {
+          var o=smartlook=function(){o.api.push(arguments)},h=d.getElementsByTagName('head')[0];
+          var c=d.createElement('script');o.api=new Array();c.async=true;c.type='text/javascript';
+          c.charset='utf-8';c.src='https://web-sdk.smartlook.com/recorder.js';h.appendChild(c);
+          })(document);
+          smartlook('init', '${process.env.NEXT_PUBLIC_SMARTLOOK_ANALYTICS}', {region: 'eu' });
+        `,
+          }}
+        />
+
         {/* <!-- Favicon --> */}
         <link rel="shortcut icon" type="image/x-icon" href="/favicon.png" />
         {/* <!-- plugin CSS --> */}
